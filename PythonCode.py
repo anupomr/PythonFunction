2604078ab52872db5ff59282098d7ff8e17a05fe
anupo <anupomr@yahoo.com>
2020-03-13 16:22:54 -0400

    Validity enforces

--- PythonCode.py
+++ PythonCode.py
@@ -1,2 +1,13 @@
-print("Hello")
+print("Python Error Handling ")
+age = -1
+while age <= 0:
+    try:
+        age = int(input('enter your age in years: '))
+        if age <= 0:
+            print('your age must be positive')
+    except(ValueError, EOFError):
+        print('invalid response')
+
+
+
 
