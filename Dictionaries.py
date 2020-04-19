c5479aff4e5614fb3d72682027bda44c03909500
anupo <anupomr@yahoo.com>
2020-04-19 08:08:54 -0400

    Emoji converter implemented

--- Dictionaries.py
+++ Dictionaries.py
@@ -14,11 +14,22 @@ phone = input(" Phone: ")
 digits_mapping = {
     "1": "One",
     "2": "Two",
-    "3": "Three",
+    "3": "Three ",
     "4": "Four"
 }
 output = ""
 for ch in phone:
-    output += digits_mapping.get(ch) + " "
+    output += digits_mapping.get(ch, "!") + " "
 print(output)
 
+# Emoji mapping
+message = input("> ")
+words = message.split(' ')
+emojis = {
+    ":)": "😊", # fro Emoji Win key + ; together
+    ":(": "😒"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
